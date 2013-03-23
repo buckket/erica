9f876641beadfd5762f8852b4b3a7884dbd2b611
MrLoom <MrLoom@soeinfachistdas.net>
2013-03-23 02:30:11 +0100

    Domian++

--- Domian/plugin.py
+++ Domian/plugin.py
@@ -38,7 +38,7 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 
-from time import mktime
+from time import mktime, time
 from datetime import datetime
 import feedparser
 
@@ -55,8 +55,8 @@ class Domian(callbacks.Plugin):
         nextshow = None
         
         for show in feed.entries:
-            showStart = datetime.fromtimestamp(mktime(show.date_parsed)).replace(hour=1)
-            showEnd = datetime.fromtimestamp(mktime(show.date_parsed)).replace(hour=2)
+            showStart = datetime.fromtimestamp(mktime(show.published_parsed)).replace(hour=1)
+            showEnd = datetime.fromtimestamp(mktime(show.published_parsed)).replace(hour=2)
             show['showstart'] = showStart
             
             if showStart < now and showEnd > now:
@@ -73,9 +73,10 @@ class Domian(callbacks.Plugin):
                             nextshow['onair'] = False
         try:
             if nextshow['onair']:
-                reply = u'Domian läuft gerade. (%s)' % nextshow.description
+                reply = u'Domian läuft gerade. (%s) - http://www.wdr.de/wdrlive/media/einslive.m3u' % nextshow.description
             else:
-                reply = u'Nächste Sendung am %s (%s)' % (nextshow['showstart'].strftime('%d.%m.%Y um %H:%M'), nextshow.description)
+                starts_in = formatTimespan(int(mktime(nextshow['showstart'].timetuple()) - time()))
+                reply = u'Nächste Sendung am %s (%s) - in %s' % (nextshow['showstart'].strftime('%d.%m.%Y um %H:%M'), nextshow.description, starts_in)
         except:
             reply = u'Noch keine Daten vorhanden!'
             
@@ -85,6 +86,103 @@ class Domian(callbacks.Plugin):
 
 
 Class = Domian
+
+
+def formatTimespan(timespan):
+   return(_formatTimespan(timespan,
+      { 'separator': ', ',
+        'format_singular': '1 %s',
+        'format_plural': '%d %s',
+        'week_singular': 'Woche',
+        'week_plural': 'Wochen',
+        'day_singular': 'Tag',
+        'day_plural': 'Tage',
+        'hour_singular': 'Stunde',
+        'hour_plural': 'Stunden',
+        'minute_singular': 'Minute',
+        'minute_plural': 'Minuten',
+        'second_singular': 'Sekunde',
+        'second_plural': 'Sekunden' }))
+
+def formatTimespanShort(timespan):
+   return(_formatTimespan(timespan,
+      { 'separator': '',
+        'format_singular': '1%s',
+        'format_plural': '%d%s',
+        'week_singular': 'w',
+        'week_plural': 'w',
+        'day_singular': 'd',
+        'day_plural': 'd',
+        'hour_singular': 'h',
+        'hour_plural': 'h',
+        'minute_singular': 'm',
+        'minute_plural': 'm',
+        'second_singular': 's',
+        'second_plural': 's' }))
+
+def _formatTimespan(timespan, m):
+
+   if (timespan < 1):
+      return(m['format_plural'] % (0, m['second_plural']))
+
+   text = ''
+   timespan = int(timespan)
+   left = timespan
+
+   weeks = left / (60**2 * 24 * 7)
+   left -= weeks * (60**2 * 24 * 7)
+
+   days = left / (60**2 * 24)
+   left -= days * (60**2 * 24)
+
+   hours = left / (60**2)
+   left -= hours * (60**2)
+
+   minutes = left / 60
+   left -= minutes * 60
+
+   seconds = left
+
+   def append_part(text, value, singular, plural):
+
+      if (value <= 0):
+         return(text)
+
+      if (len(text) > 0):
+         text += m['separator']
+
+      if (value == 1):
+         text += m['format_singular'] % (singular)
+
+      else:
+         text += m['format_plural'] % (value, plural)
+
+      return(text)
+
+   # weeks (always include)
+
+   text = append_part(text, weeks, m['week_singular'], m['week_plural'])
+
+   # days (always include)
+
+   text = append_part(text, days, m['day_singular'], m['day_plural'])
+
+   # hours (if < 1 week)
+
+   if (timespan < (60**2 * 24 * 7)):
+      text = append_part(text, hours, m['hour_singular'], m['hour_plural'])
+
+   # minutes (if < 1 day)
+
+   if (timespan < (60**2 * 24)):
+      text = append_part(text, minutes, m['minute_singular'], m['minute_plural'])
+
+   # seconds (if < 1 hour)
+
+   if (timespan < (60**2)):
+      text = append_part(text, seconds, m['second_singular'], m['second_plural'])
+
+   return(text)
 
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
